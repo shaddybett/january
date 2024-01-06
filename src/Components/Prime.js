1279733077c2a01a1d87a299db195c6d89e34742
Shaddy <shadybett540@gmail.com>
2024-01-06 13:38:33 +0300

    Made three components

---
+++ src/Components/Prime.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Prime() {
+  return (
+    <div>
+        <h1>Hello this is prime</h1>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import About from './Components/About';
+import Home from './Components/Home';
+import Prime from './Components/Prime';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <About/>
+    <Home/>
+    <Prime/>
   </React.StrictMode>
 );
