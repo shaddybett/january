c4f1f14a4d190d21466461bfbcba308413e33d78
Shaddy <shadybett540@gmail.com>
2024-01-06 13:49:08 +0300

    installed routing tools

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from './App';
 import About from './Components/About';
-import Home from './Components/Home';
+import Home from "./Components/Home";
 import Prime from './Components/Prime';
+import { Router } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
     <About/>
-    <Home/>
+    <Home />
     <Prime/>
   </React.StrictMode>
 );
