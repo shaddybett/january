0c65d101517b62ad2d889234635f4eb4a2861461
Shaddy <shadybett540@gmail.com>
2024-01-07 11:58:23 +0300

    done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div>
-
       <Link to="/about">About</Link>
       <h1>Sign up or log in</h1>
+      
     </div>
   );
 }
